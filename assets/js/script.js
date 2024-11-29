28131770adf23e7e92d8ff6efac3ccfb5f682b9b
kbluuzy <manonk1@tcnj.edu>
2024-11-29 16:39:32 -0500

    updat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 console.log("hello world");
+
+if(jQuery) {
+console.log("jQuery loaded");
+}
+
+else {
+    console.log("no query");
+}
+$('.faq-question').click(function() {
+ $(this).children('p').slideToggle(500)
+ $(this).children('button').toggleClass('closed open')
+});
